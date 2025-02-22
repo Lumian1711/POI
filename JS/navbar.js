f70a4bdc286c3126ebb536572953576e34544a15
Lumian1711 <Lumian1711>
2025-02-21 19:02:42 -0600

    Menu

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,11 @@
 const book = document.getElementById('book');
 const lateral = document.querySelector('.barra');
-const span = document.querySelector('span');
+const spans = document.querySelectorAll('span');
 
 book.addEventListener('click', ()=>{
     lateral.classList.toggle("mini-lateral");
-    span.classList.toggle("oculto");
+    
+    spans.forEach((span)=>{
+        span.classList.toggle("oculto");
+    });
 });
