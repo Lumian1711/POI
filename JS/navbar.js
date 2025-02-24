6cbf09f162f2d62c3824bde0d1978137c74265ca
Lumian1711 <Lumian1711>
2025-02-24 04:25:34 -0600

    Paginas de navegacion

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,13 +1,12 @@
 const book = document.getElementById('book');
 const lateral = document.querySelector('.barra');
 const spans = document.querySelectorAll('span');
-const main = document.querySelectorAll('main');
+const main = document.querySelector('main');
 
-book.addEventListener('click', ()=>{
+book.addEventListener('click',()=>{
     lateral.classList.toggle("lateral");
+    main.classList.toggle("grande");
     spans.forEach((span)=>{
         span.classList.toggle("oculto");
     });
-    main.classList.toggle("min-main");
-
 });
---
+++ JS/prin.js
@@ -0,0 +1,4 @@
+/*const main = document.querySelector('main');
+book.addEventListener('click',()=>{
+    main.classList.toggle("grande");
+});*/
