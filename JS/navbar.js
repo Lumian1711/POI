e6b6e70ec5208243bfc856449b8a95b79c85ffd8
Lumian1711 <Lumian1711>
2025-02-24 03:01:55 -0600

    Prueba

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,11 +1,13 @@
 const book = document.getElementById('book');
 const lateral = document.querySelector('.barra');
 const spans = document.querySelectorAll('span');
+const main = document.querySelectorAll('main');
 
 book.addEventListener('click', ()=>{
     lateral.classList.toggle("lateral");
-    
     spans.forEach((span)=>{
         span.classList.toggle("oculto");
     });
+    main.classList.toggle("min-main");
+
 });
